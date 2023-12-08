f8f864f276a3f16d3feca6b7b56d007419b7459a
alison <adevpr11@gmail.com>
2023-12-08 17:12:41 -0300

    Criação do Producer e Consumer

--- compra/src/main/java/com/mscompra/model/Pedido.java
+++ compra/src/main/java/com/mscompra/model/Pedido.java
@@ -3,6 +3,7 @@ package com.mscompra.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,8 +11,11 @@ import javax.persistence.Id;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
 import lombok.*;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -22,7 +26,10 @@ import java.util.Date;
 @Builder
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @Entity(name = "tb_pedido")
-public class Pedido {
+public class Pedido implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 7335789400519836017L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- compra/src/main/java/com/mscompra/service/PedidoService.java
+++ compra/src/main/java/com/mscompra/service/PedidoService.java
@@ -2,17 +2,22 @@ package com.mscompra.service;
 
 import com.mscompra.model.Pedido;
 import com.mscompra.repository.PedidoRepository;
+import com.mscompra.service.rabbitmq.Producer;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PedidoService {
     private final PedidoRepository repository;
+    private final Producer producer;
 
-    public PedidoService(PedidoRepository repository) {
+    public PedidoService(PedidoRepository repository, Producer producer) {
         this.repository = repository;
+        this.producer = producer;
     }
 
     public Pedido save(Pedido pedido) {
-        return repository.save(pedido);
+        pedido = repository.save(pedido);
+        producer.producer(pedido);
+        return pedido;
     }
 }
